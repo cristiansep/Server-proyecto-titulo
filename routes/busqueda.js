9496f192a3645c4a7e90e6380893636de825f5c6
cristiansep <cristiansepulvedamendez@gmail.com>
2019-12-16 16:21:05 -0300

    Ruta de busquedas lista

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -2,41 +2,127 @@ const express = require('express');
 
 const Hospital = require('../models/hospital');
 const Medico = require('../models/medico');
+const Usuario = require('../models/usuario');
 
 
 const app = express();
 
-app.get('/todo/:busqueda',(req,res,next) => {
 
-    let busqueda = req.params.busqueda;
-    let regex = new RegExp(busqueda, 'i');
 
-    buscarHospitales(busqueda, regex).then(hospitales => {
-      res.status(200).json({
-        ok: true,
-        hospitales
+//--------------------------//
+//  Busqueda pro colecion   //
+//-------------------------//
+app.get("/coleccion/:tabla/:busqueda", (req, res) => {
+
+  let busqueda = req.params.busqueda;
+  let tabla = req.params.tabla;
+  let regex = new RegExp(busqueda, "i");
+
+  let promesa;
+
+  switch (tabla) {
+    case "usuarios":
+      promesa = buscarUsuarios(busqueda, regex);
+      break;
+
+    case "medicos":
+      promesa = buscarMedicos(busqueda, regex);
+      break;
+
+    case "hospitales":
+      promesa = buscarHospitales(busqueda, regex);
+      break;
+
+    default:
+      return res.status(400).json({
+        ok: false,
+        mensaje: 'Parametros de busqueda incorrectos'
       });
+  }
+
+
+  promesa.then(data =>{
+    res.status(200).json({
+      ok: true,
+      [tabla]: data
     });
-
-  
-    function buscarHospitales(busqueda, regex) {
-
-      return new Promise((resolve, reject) => {
-
-        Hospital.find({ nombre: regex }, (err, hospitales) => {
-
-            if(err){
-                reject('Error al cargar hospitales');
-            }else {
-                resolve(hospitales)
-            }
+  })
 
 
-        });
 
-      });
-    }
-    
 });
+
+
+//--------------------------//
+//  Busqueda general        //
+//-------------------------//
+app.get("/todo/:busqueda", (req, res, next) => {
+
+  let busqueda = req.params.busqueda;
+  let regex = new RegExp(busqueda, "i");
+
+  Promise.all([
+        buscarHospitales(busqueda, regex), 
+        buscarMedicos(busqueda, regex),
+        buscarUsuarios(busqueda, regex)
+        ])
+        .then(respuestas =>{
+            res.status(200).json({
+              ok: true,
+              hospitales: respuestas[0],
+              medicos: respuestas[1],
+              usuarios: respuestas[2]
+            });
+        })
+
+});
+
+function buscarHospitales(busqueda, regex) {
+
+  return new Promise((resolve, reject) => {
+
+    Hospital.find({ nombre: regex })
+      .populate("usuario", "nombre email")
+      .exec((err, hospitales) => {
+        if (err) {
+          reject("Error al cargar hospitales");
+        } else {
+          resolve(hospitales);
+        }
+      });
+  });
+}
+
+function buscarMedicos(busqueda, regex) {
+  return new Promise((resolve, reject) => {
+
+    Medico.find({ nombre: regex })
+          .populate('usuario', 'nombre email')
+          .populate('hospital')
+          .exec((err, medicos) => {
+      if (err) {
+        reject("Error al cargar medicos");
+      } else {
+        resolve(medicos);
+      }
+    });
+  });
+}
+
+function buscarUsuarios(busqueda, regex) {
+  return new Promise((resolve, reject) => {
+
+    Usuario.find({}, 'nombre email role')
+           .or([{'nombre': regex}, {'email': regex}])
+           .exec((err, usuarios) =>{
+             if(err){
+               reject('Error al cargar usuario',err);
+             }else {
+               resolve(usuarios)
+             }
+           })
+
+  });
+}
 
 module.exports = app;
